7b89a704f0d42c674c35e40f675414b053698fc5
Timothy Roch <timothyroch@gmail.com>
2025-06-25 21:20:20 -0400

    Update Grid.h

--- include/Grid.h
+++ include/Grid.h
@@ -1,1 +1,30 @@
+#ifndef GRID_H
+#define GRID_H
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+struct Tile {
+    sf::RectangleShape shape;
+    bool occupied;
+
+    Tile(float x, float y, float size);
+};
+
+class Grid {
+public:
+    Grid(int rows, int cols, float tileSize);
+
+    void render(sf::RenderWindow& window);
+    bool isTileEmpty(int row, int col) const;
+    void occupyTile(int row, int col);
+    sf::Vector2i getTileFromMouse(sf::Vector2i mousePos);
+
+private:
+    std::vector<std::vector<Tile>> tiles;
+    int rows, cols;
+    float tileSize;
+};
+
+#endif
 
